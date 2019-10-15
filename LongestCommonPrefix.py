3eb797db86267764af9305224f576a1a67f0f3a2
sidharth3 <sidhu.beena@gmail.com>
2019-10-15 19:10:45 +0800

    Create LongestCommonPrefix.py

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,12 @@
+#Write a function to find the longest common prefix string amongst an array of strings.
+#If there is no common prefix, return an empty string "".
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        lcp = ""
+        for i in zip(*strs):
+            if(len(set(i)))>1:
+                break
+            lcp+=i[0]
+        return lcp
+            
