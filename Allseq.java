80308824634ddd6d25c3e1641c7198b1bcbbf106
Sidharth <sidhu.beena@gmail.com>
2019-11-03 22:57:01 +0800

    Update Allseq.java

--- Allseq.java
+++ Allseq.java
@@ -1,18 +1,11 @@
 import java.util.ArrayList;
 
 class Allseq {
-    // The method that prints all
-    // possible strings of length k.
-    // It is mainly a wrapper over
-    // recursive function printAllKLengthRec()
     static void printAllKLength(char[] set, int k) {
         int n = set.length;
         printAllKLengthRec(set, "", n, k);
     }
 
-    // The main recursive method
-    // to print all possible
-    // strings of length k
     static void printAllKLengthRec(char[] set, String prefix, int n, int k) {
 
         // Base case: k is 0,
@@ -22,17 +15,11 @@ class Allseq {
             return;
         }
 
-        // One by one add all characters
-        // from set and recursively
-        // call for k equals to k-1
         for (int i = 0; i < n; ++i) {
             System.out.print("value of i is");
             System.out.println(i);
             // Next character of input added
             String newPrefix = prefix + set[i];
-
-            // k is decreased, because
-            // we have added a new character
             printAllKLengthRec(set, newPrefix, n, k - 1);
         }
     }
